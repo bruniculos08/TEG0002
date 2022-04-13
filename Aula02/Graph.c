6a29cae41ec5bdeb639b808b080b1f6bee6af7c9
Bruno Rafael dos Santos <62629549+bruniculos08@users.noreply.github.com>
2022-04-13 11:08:02 -0300

    update

---
+++ Aula02/Graph.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "headGraph.h"
+
+int main(){
+    listaDeAdjacencia *graph = createGraph();
+}
--- Aula02/headGraph.c
+++ Aula02/headGraph.c
@@ -5,16 +5,27 @@
 listaDeAdjacencia *createGraph(){
     listaDeAdjacencia *graph;
     graph = NULL;
+    listaDeAdjacencia *listaAux;
+    listaAux = graph;
     int n;
     int a;
-    printf("Digite o número de vertices: ");
+    printf("Digite o numero de vertices: ");
     scanf("%i", &n);
     for(int i = 0; i < n; i++){
         if(graph == NULL) graph = (listaDeAdjacencia *)malloc(sizeof(listaDeAdjacencia));
+        graph->vertices = NULL;
+        graph->next = NULL;
         printf("Digite o numero de vertices adjacentes ao vertice v[%i]: ", i+1);
         scanf("%i", &a);
+        listaDeVertices *aux;
+        aux = graph->vertices;
         for(int j = 0; j < a; j++){
+            if(aux == NULL) aux = (listaDeVertices *)malloc(sizeof(listaDeVertices));
+            aux->next = NULL;
             printf("Digite o numero do vertice %i adjacente a v[%i]: ", j+1, i+1);
+            scanf("%i", &aux->indice);
+            aux = aux->next;
         }
+        graph = graph->next;
     }
 }
