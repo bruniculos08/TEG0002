e6ce9ec9e623f671c13f0e56f0de788dd3a3d534
Bruno Rafael dos Santos <62629549+bruniculos08@users.noreply.github.com>
2022-04-17 13:04:29 -0300

    update

--- Aula02/headGraph.c
+++ Aula02/headGraph.c
@@ -7,14 +7,21 @@ listaDeAdjacencia *createGraph(listaDeAdjacencia *graph, int n){
     graphAux = graph;
     for(int i = 0; i <n; i++){
         if(graphAux == NULL){
+            // Criando a primeira linha da matriz de adjacências 
             graphAux = (listaDeAdjacencia *)malloc(sizeof(listaDeAdjacencia));
             graphAux->vertices = NULL;
+            graphAux->nextLista = NULL;
         }
+        
+        // O número de vértices é igual a quantidade de "colunas" da linha
         int nVertices;
         printf("Digite o numero de vertices adjacentes ao vertice v[%i]: ", i);
         scanf("%d", &nVertices);
+        
+        // Ponteiro para andar pelas colunas da linha
         listaDeVertices *verticesAux;
         verticesAux = graphAux->vertices;
+
         for(int j = 0; j < nVertices; j++){
             if(verticesAux == NULL){
                 verticesAux = (listaDeVertices *)malloc(sizeof(listaDeVertices));
@@ -22,8 +29,11 @@ listaDeAdjacencia *createGraph(listaDeAdjacencia *graph, int n){
             }
             printf("Digite o indice do vertice %i adjacente a v[%i]: ", j+1, i+1);
             scanf("%i", &verticesAux->indice);
+            // Anda para a próxima coluna da linha
+            verticesAux = verticesAux->nextVertice;
         }
     }
+    return graphAux;
 }
 
 int degree(int k, listaDeAdjacencia *graph){
