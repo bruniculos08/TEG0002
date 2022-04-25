e9251e6cbb5f0b3fba7521e23ebb9bccc73049dc
Bruno Rafael dos Santos <62629549+bruniculos08@users.noreply.github.com>
2022-04-25 09:40:56 -0300

    update

--- Trabalho01/headGraph.c
+++ Trabalho01/headGraph.c
@@ -12,23 +12,40 @@ int **createMatrix(int rows, int columns){
     // - cada ponteiro para int aponta para um espaço a partir de onde há um número columns de int's
     for(int i=0; i<rows; i++){
         matrix[i] = (int *)malloc(columns*sizeof(int));
+
+        // (2.1) Setando os elementos da matriz iguais à zero para inserção (por meio de adição) das arestas na função createGraph:
+        for(int j=0; j<columns; j++) matrix[i][j] = 0;
     }
     return matrix;
 }
 
 int **createGraph(int n){
     
-    // (1) Criando a matriz do grafo:
+    // (1) Criando a matriz de adjacência do grafo:
     int **matrix = createMatrix(n, n);
-    printf("tamanho da matriz = %i\n", sizeof(matrix));
-    
-    int i = 0;
-    int j = 0;
+
     // (2) Loop de acordo para inserir as arestas:
+    int i = -1, j = -1;
     do
     {
-      scanf("(%i, %i)", &i, &j);  
-    } while (i != 0 && j != 0);
+      // (2.1) Se não for utilizado fflush(stdin) ocorre bug:
+      fflush(stdin);
+      scanf("(%i, %i)", &i, &j);
+
+      // (2.2) Fim da inserção de arestas:
+      if(i == -1 || j == -1 || i > n || j > n) break;
+
+      // (2.3) Caso seja um lacete:
+      if(i == j) matrix[i-1][j-1] += 2;
+
+      // (2.4) Caso seja uma aresta normal:
+      // - há a adição nos elementos simétricos da matriz (tendo em vista que a matriz é simétrica):
+      else{
+          matrix[i-1][j-1] += 1;
+          matrix[j-1][i-1] += 1;
+      }
+      
+    } while (i != -1 && j != -1);
     
     return matrix;
 }
@@ -42,11 +59,6 @@ int degree(int i, int **matrix, int n){
 }
 
 void printMatrix(int **matrix, int rows, int columns){
-    //rows = (sizeof(matrix));
-    printf("rows = %i\n", rows);
-    //columns = sizeof(matrix[0])/sizeof(matrix[0][0]);
-    printf("columns = %i\n", columns);
-
     for(int i=0; i<rows; i++){
         for(int j=0; j<columns; j++) printf("%i ", matrix[i][j]);
         printf("\n");
