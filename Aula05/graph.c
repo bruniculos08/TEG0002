c856e34edffc38c4e09ec871a1defb79c75e9cd3
Bruno Rafael dos Santos <62629549+bruniculos08@users.noreply.github.com>
2022-05-18 09:38:56 -0300

    update

---
+++ Aula05/graph.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void search(int **image, int minRow, int maxRow){
+
+}
+
+int fileHeight(){
+    int height = 0;
+    return height;
+}
+int fileWidth(){
+    int width = 0;
+    return width;
+}
+
+int main(){
+    int **image;
+    image = (int**)malloc(sizeof(int*)*fileHeight());
+    *image = (int*)malloc(sizeof(int)*fileHeight());
+}
