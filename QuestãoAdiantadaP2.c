7e0acb9393ffb99d9fe2f26adb552700d1d9fc80
Bruno Rafael dos Santos <62629549+bruniculos08@users.noreply.github.com>
2022-07-21 21:36:13 -0300

    update

--- QuestãoAdiantadaP2.c
+++ QuestãoAdiantadaP2.c
@@ -26,12 +26,10 @@ int min(int a, int b){
     else return b;
 }
 
-int fordFulkerson(int **matriz, int n, int **capacidade, int **fluxo){
-    
-    matriz = zeraMatriz(matriz, n);
+int fordFulkerson(int **W, int n, int **C, int **fluxo){
+    fluxo = zeraMatriz(fluxo, n);
     marca vetorDeMarcas[n];
     int vetorVisitados[n];
-    fila *filaDeVertices = NULL;
     
     for(int i=0; i<n; i++){
         vetorDeMarcas[i].verticeAntecessor = -1;
@@ -39,31 +37,25 @@ int fordFulkerson(int **matriz, int n, int **capacidade, int **fluxo){
         vetorVisitados[i] = 0;
     }
     
-    vetorDeMarcas[0].deltaFluxo = inf; 
-
     do{
+        vetorDeMarcas[0].deltaFluxo = inf; 
+
         for (int i = 0; i < n; i++){
             if((vetorDeMarcas[i].verticeAntecessor != -1 || vetorDeMarcas[i].deltaFluxo != 0) && vetorVisitados[i] == 0){
-        
                 for(int j = 0; j < n; j++){
-                    
-                    if((vetorDeMarcas[j].verticeAntecessor != -1 || vetorDeMarcas[j].deltaFluxo != 0) && matriz[i][j] != 0){
-                        
-                        if(matriz[i][j] == 1 && fluxo[i][j] < capacidade[i][j]){
+                    if((vetorDeMarcas[j].verticeAntecessor != -1 || vetorDeMarcas[j].deltaFluxo != 0) && W[i][j] != 0){    
+                        if(W[i][j] == 1 && fluxo[i][j] < C[i][j]){
                             vetorDeMarcas[j].verticeAntecessor = i;
-                            vetorDeMarcas[j].deltaFluxo = min(vetorDeMarcas[i].deltaFluxo, capacidade[i][j]-fluxo[i][j]);
+                            vetorDeMarcas[j].deltaFluxo = min(vetorDeMarcas[i].deltaFluxo, C[i][j]-fluxo[i][j]);
                         }
-
-                        else if(matriz[i][j] == -1 && fluxo[i][j] < capacidade[i][j]){
+                        else if(W[i][j] == -1 && fluxo[i][j] < C[i][j]){
                             vetorDeMarcas[j].verticeAntecessor = i;
                             vetorDeMarcas[j].deltaFluxo = min(vetorDeMarcas[i].deltaFluxo, fluxo[i][j]);
-                        }
-                        
+                        }                   
                     }
                 }
             }
         }
-
         int z = n-1;
         do
         {
@@ -71,33 +63,32 @@ int fordFulkerson(int **matriz, int n, int **capacidade, int **fluxo){
                 for(int i=0; i<n; i++){
                     vetorDeMarcas[i].verticeAntecessor = -1;
                     vetorDeMarcas[i].deltaFluxo = 0;
+                    vetorVisitados[i] = 0;
                 }
                 break;
             }
 
             int y = vetorDeMarcas[z].verticeAntecessor;
 
-            if(matriz[y][z] == 1){
+            if(W[y][z] == 1){
                 fluxo[y][z] == fluxo[y][z] + vetorDeMarcas[n-1].deltaFluxo;
                 fluxo[z][y] == fluxo[y][z] + vetorDeMarcas[n-1].deltaFluxo;
             }
-
             else{
                 fluxo[y][z] == fluxo[y][z] - vetorDeMarcas[n-1].deltaFluxo;
                 fluxo[z][y] == fluxo[y][z] - vetorDeMarcas[n-1].deltaFluxo;
             }
 
             z = y;
-
         }while (z != 0);
         
     } while(vetorDeMarcas[n-1].verticeAntecessor == -1);
 
     int fluxoMaximo = 0;
     for (int i=0; i<n; i++){
-        if(matriz[0][i] == 1){
+        if(W[0][i] == 1){
             fluxoMaximo += fluxo[0][i];        
-        }
+            }
     }
     return fluxoMaximo;
 }
